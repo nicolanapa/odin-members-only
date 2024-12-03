e461539862ac4762c6adf30c957ee052ea9bbc17
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-03 22:02:02 +0100

    Added app template with initialized express-session and passport

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import process from "process";
+import url from "url";
+import path from "path";
+import session from "express-session";
+import passport from "passport";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(session({ secret: process.env.SECRET_SESSION, resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname + "/styles")));
+app.use(express.static(path.join(__dirname + "/scripts")));
+
+app.get("/", (req, res) => {
+    res.send("<h1>Home</h1>");
+});
+
+app.listen(PORT);
