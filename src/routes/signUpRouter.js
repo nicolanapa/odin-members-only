2663df20c4c7790e394b3c4356ac68381c9c5e29
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-04 22:33:12 +0100

    Added validation for the signUp Form
    Added a custom validator for 'confirmPassword' to check if it equals to 'password'

--- src/routes/signUpRouter.js
+++ src/routes/signUpRouter.js
@@ -1,12 +1,44 @@
 import { Router } from "express";
-import { body } from "express-validator";
+import { body, validationResult } from "express-validator";
+
+const passwordChecker = (value, { req }) => {
+    return value === req.body.password;
+};
 
 const signUpValidator = [
-    body("first_name"),
-    body("last_name"),
-    body("email"),
-    body("password"),
-    body("confirmPassword"),
+    body("first_name")
+        .trim()
+        .escape()
+        .notEmpty()
+        .withMessage("First Name Should Not Be Empty")
+        .isAlpha()
+        .withMessage("Only Characters Allowed in First Name"),
+    body("last_name")
+        .trim()
+        .escape()
+        .notEmpty()
+        .withMessage("Last Name Should Not Be Empty")
+        .isAlpha()
+        .withMessage("Only Characters Allowed in Last Name"),
+    body("email")
+        .trim()
+        .escape()
+        .notEmpty()
+        .withMessage("Email Should Not Be Empty")
+        .isEmail()
+        .withMessage("Email is not an email"),
+    body("password")
+        .notEmpty()
+        .withMessage("Password Should Not Be Empty")
+        .isLength({ min: 4, max: 96 })
+        .withMessage("Password should have a length between 4 and 96"),
+    body("confirmPassword")
+        .notEmpty()
+        .withMessage("Password Should Not Be Empty")
+        .isLength({ min: 4, max: 96 })
+        .withMessage("Password should have a length between 4 and 96")
+        .custom(passwordChecker)
+        .withMessage("Confirm Password isn't the same as Password"),
 ];
 
 const signUpRouter = Router();
@@ -18,7 +50,13 @@ signUpRouter.get("/", (req, res) => {
 signUpRouter.post("/", [
     signUpValidator,
     async (req, res) => {
-        //
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            console.log(errors.array());
+
+            return;
+        }
     },
 ]);
 
