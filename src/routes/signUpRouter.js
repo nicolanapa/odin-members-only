cde43c1a944d74da00c8bf3fc928dd9c091f3426
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-04 21:58:13 +0100

    Added POST / with empty body validations in signUpValidator

--- src/routes/signUpRouter.js
+++ src/routes/signUpRouter.js
@@ -1,9 +1,25 @@
 import { Router } from "express";
+import { body } from "express-validator";
+
+const signUpValidator = [
+    body("first_name"),
+    body("last_name"),
+    body("email"),
+    body("password"),
+    body("confirmPassword"),
+];
 
 const signUpRouter = Router();
 
 signUpRouter.get("/", (req, res) => {
     res.status(200).render("./signUp");
 });
+
+signUpRouter.post("/", [
+    signUpValidator,
+    async (req, res) => {
+        //
+    },
+]);
 
 export { signUpRouter };
