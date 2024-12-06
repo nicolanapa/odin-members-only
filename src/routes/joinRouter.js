15ec12861b8dfc4a33f662057c548c700862449e
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-06 22:29:19 +0100

    Changed updateStatus id paramaterer and added checking for if user is logged in

--- src/routes/joinRouter.js
+++ src/routes/joinRouter.js
@@ -10,10 +10,14 @@ joinRouter.get("/", (req, res) => {
 
 joinRouter.post("/", async (req, res) => {
     if (req.body.passcode === process.env.SECRET_PASSWORD) {
-        updateStatus("member", 0);
+        if (!req.user) {
+            return res.status(401).render("./join", { error: "Not logged in" });
+        }
+
+        updateStatus("member", req.user.id);
     } else {
         res.status(401).render("./join", { error: "Wrong passcode" });
-        
+
         return;
     }
 
