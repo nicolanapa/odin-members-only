257f79b4cef21b77b4a8e9f886b76bdd4b9d01db
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-06 21:45:49 +0100

    Added GET / and POST / to update membership status

---
+++ src/routes/joinRouter.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import process from "process";
+import { updateStatus } from "../db/updateStatus.js";
+
+const joinRouter = Router();
+
+joinRouter.get("/", (req, res) => {
+    res.status(200).render("./join");
+});
+
+joinRouter.post("/", async (req, res) => {
+    if (req.body.passcode === process.env.SECRET_PASSWORD) {
+        updateStatus("member", 0);
+    } else {
+        res.status(401).render("./join", { error: "Wrong passcode" });
+    }
+
+    res.status(200).redirect("/");
+});
+
+export { joinRouter };
