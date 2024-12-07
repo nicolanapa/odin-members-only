efbd7220b6007a28831de472760d3f747916f56f
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-07 13:45:53 +0100

    Added GET / and POST / to insert a Message if authenticated

---
+++ src/routes/createMessageRouter.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import { insertMessage } from "../db/insertMessage.js";
+
+const createMessageRouter = Router();
+
+createMessageRouter.get("/", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.status(200).render("./createMessage");
+    } else {
+        res.status(401).render("./login", {
+            errors: [{ msg: "401 Unauthorized: Log in first before creating a new message" }],
+        });
+    }
+});
+
+createMessageRouter.post("/", async (req, res) => {
+    const { title, content } = req.body;
+
+    if (req.isAuthenticated()) {
+        await insertMessage(title, content, req.user.id);
+    } else {
+        res.status(401).redirect("/");
+
+        return;
+    }
+
+    res.status(200).redirect("/");
+});
+
+export { createMessageRouter };
