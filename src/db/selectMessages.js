39275846318f7e30f4899fd5045759f010a28d34
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-08 14:16:09 +0100

    Changed selectMessages to fetch all message and only email from username

--- src/db/selectMessages.js
+++ src/db/selectMessages.js
@@ -1,9 +1,11 @@
 import pool from "./pool.js";
 
 const selectMessages = async (limit = 50) => {
-    await pool.query(
+    return await pool.query(
         `
-        SELECT * FROM message
+        SELECT message.*, username.email FROM message
+        INNER JOIN username
+        ON message.username_id = username.id
         LIMIT $1;
         `,
         [limit],
