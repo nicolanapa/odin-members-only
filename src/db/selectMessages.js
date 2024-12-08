34b5b7187d070a4ec9b88153384b78fbf3ed1e40
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-08 14:35:59 +0100

    Added formatting of posted_date

--- src/db/selectMessages.js
+++ src/db/selectMessages.js
@@ -3,7 +3,7 @@ import pool from "./pool.js";
 const selectMessages = async (limit = 50) => {
     return await pool.query(
         `
-        SELECT message.*, username.email FROM message
+        SELECT message.*, TO_CHAR(posted_date, 'DD/MM/YYYY HH24:MI:SS TZ') AS posted_date, username.email FROM message
         INNER JOIN username
         ON message.username_id = username.id
         LIMIT $1;
