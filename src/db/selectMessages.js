0f1a5998482e581b58714c47c695cf1cbc1978ce
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-11 22:29:39 +0100

    Added selectMessage to get a specific Message for modifyMessage

--- src/db/selectMessages.js
+++ src/db/selectMessages.js
@@ -1,5 +1,17 @@
 import pool from "./pool.js";
 
+const selectMessage = async (id) => {
+    return await pool.query(
+        `
+        SELECT message.*, TO_CHAR(posted_date, 'DD/MM/YYYY HH24:MI:SS TZ') AS posted_date, username.email FROM message
+        INNER JOIN username
+        ON message.username_id = username.id
+        WHERE message.id = $1;
+        `,
+        [id],
+    );
+};
+
 const selectMessages = async (limit = 50) => {
     return await pool.query(
         `
@@ -12,4 +24,4 @@ const selectMessages = async (limit = 50) => {
     );
 };
 
-export { selectMessages };
+export { selectMessage, selectMessages };
